d0454c1650179639ba19ce790bbd87746fce3036
evgostep <evgostep@gmail.com>
2019-10-25 11:09:37 +0000

    Update openbox_deb10.sh

--- openbox_deb10.sh
+++ openbox_deb10.sh
@@ -5,6 +5,6 @@
 
 ######## Script ###################
 #!/bin/bash
-sudo apt -y install openbox obconf obmenu lxterminal nano net-tools lightdm pcmafm tint2 nitrogen gnome-background mate-background menu
+sudo apt -y install openbox obconf obmenu lxterminal nano net-tools lightdm pcmafm tint2 nitrogen gnome-background mate-background menu mousepad
 mkdir ~/.config/openbox
 echo -e "nitrogen --restore &\ntint2 &" > ~/.config/openbox/autostart.sh
