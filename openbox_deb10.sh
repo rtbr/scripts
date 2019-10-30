ad32b26d8d6ac8de099df24a664ebb17ac031b72
evgostep <evgostep@gmail.com>
2019-10-30 03:47:27 +0000

    Add fbxkb to see indicator of layout switch.

--- openbox_deb10.sh
+++ openbox_deb10.sh
@@ -5,6 +5,6 @@
 
 ######## Script ###################
 #!/bin/bash
-sudo apt -y install openbox obconf obmenu lxterminal nano net-tools lightdm pcmafm tint2 nitrogen gnome-background mate-background menu mousepad
+sudo apt -y install openbox obconf obmenu lxterminal nano net-tools lightdm pcmafm tint2 nitrogen gnome-background mate-background menu mousepad fbxkb
 mkdir ~/.config/openbox
-echo -e "nitrogen --restore &\ntint2 &" > ~/.config/openbox/autostart.sh
+echo -e "nitrogen --restore &\ntint2 &\nfbxkb &\nsetxkbmap -layout "us,ru" -option "grp:ctrl_shift_toggle,lv3:switch,terminate:ctrl_alt_bksp,grp_led:scroll" &" > ~/.config/openbox/autostart.sh
