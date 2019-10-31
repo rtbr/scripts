f996d3143d3a0f54bd59c0771e710e6cfec1c821
evgostep <evgostep@gmail.com>
2019-10-31 08:26:26 +0000

    Update 1cbase_unpublic.sh

--- 1cbase_public_debian/1cbase_unpublic.sh
+++ 1cbase_public_debian/1cbase_unpublic.sh
@@ -7,7 +7,7 @@
 #   -delete: удалить публикацию
 #websrv
 #   -apache2: публикация Веб-клиента для Apache 2.0
-#   -apache22: публикация Веб-клиента для Apache 2.2
+#   -apache24: публикация Веб-клиента для Apache 2.4
 #   -wsdir VirtualDir: виртуальный каталог
 #   -dir Dir: физический каталог, в который будет отображен виртуальный
 #   -descriptor vrdPath: путь к существующему vrd файлу
@@ -16,7 +16,7 @@
 #
 ########################################################################
 
-sudo -u root /opt/1C/v8.3/x86_64/webinst -delete -apache24 -wsdir $1 -dir /var/www/$1 -connstr  'Srvr="1c-01-win";Ref="$1";$
+sudo -u root /opt/1C/v8.3/x86_64/webinst -delete -apache24 -wsdir $1 -dir /var/www/$1 -connstr  'Srvr="1c-01-win";Ref="$1";' -confpath /etc/apache2/sites-available/$1.conf
 sudo -u root a2dissite $1
 rm -Rf /var/www/$1 /etc/apache2/sites-available/$1.conf /etc/apache2/sites-available/$1.conf.bak
 sudo -u root systemctl reload apache2
