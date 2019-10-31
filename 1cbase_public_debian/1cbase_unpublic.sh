a816181435eabcd92cf43bfffa80fc4428e595fa
evgostep <evgostep@gmail.com>
2019-10-31 08:23:14 +0000

    Add new file

---
+++ 1cbase_public_debian/1cbase_unpublic.sh
@@ -0,0 +1,22 @@
+################## how to use ##################
+# /root/scripts/1cbase_unpublic.sh "BASE_NAME"
+################################################
+################## keys ########################
+#
+#   -publish: опубликовать, ключ по умолчанию
+#   -delete: удалить публикацию
+#websrv
+#   -apache2: публикация Веб-клиента для Apache 2.0
+#   -apache22: публикация Веб-клиента для Apache 2.2
+#   -wsdir VirtualDir: виртуальный каталог
+#   -dir Dir: физический каталог, в который будет отображен виртуальный
+#   -descriptor vrdPath: путь к существующему vrd файлу
+#   -connstr connStr: строка соединения ИБ
+#   -confPath confPath: полный путь к конфигурационному файлу
+#
+########################################################################
+
+sudo -u root /opt/1C/v8.3/x86_64/webinst -delete -apache24 -wsdir $1 -dir /var/www/$1 -connstr  'Srvr="1c-01-win";Ref="$1";$
+sudo -u root a2dissite $1
+rm -Rf /var/www/$1 /etc/apache2/sites-available/$1.conf /etc/apache2/sites-available/$1.conf.bak
+sudo -u root systemctl reload apache2
